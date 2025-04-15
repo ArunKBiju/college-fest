dd9bd8ad35fe170348e3655d1965c2b20dc63a4f
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2025-04-15 18:21:42 +0530

    Add files via upload

---
+++ payment/group-events.js
@@ -0,0 +1,23 @@
+const groupEventForm = document.forms['groupEventForm'];
+const eventFieldGroup = document.getElementById("event");
+
+const urlParamsGroup = new URLSearchParams(window.location.search);
+const eventNameGroup = urlParamsGroup.get("event");
+if (eventNameGroup) {
+    eventFieldGroup.value = eventNameGroup;
+}
+
+groupEventForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(groupEventForm);
+    formData.append("teamLeaderName", groupEventForm.teamLeaderName.value);
+    formData.append("playerNames", groupEventForm.playerNames.value);
+
+    fetch(groupEventForm.action, { method: 'POST', body: formData, mode: "no-cors" })
+    .then(() => {
+        alert("Thank you! Your group registration details are submitted.");
+        groupEventForm.reset();
+    })
+    .catch(error => console.error('Error!', error.message));
+});
